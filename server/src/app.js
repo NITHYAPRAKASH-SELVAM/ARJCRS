9fbfbc25d966fb491eb22ff8e69473a7a38f4c33
praveen9655 <praveen965592@gmail.com>
2024-04-19 08:08:53 +0530

    U4

--- server/src/app.js
+++ server/src/app.js
@@ -15,18 +15,20 @@ mongoose
   .connect('mongodb+srv://arjcrs:CJWeBLqmzlnAaUBh@arjcrs.vfpvp3w.mongodb.net/')
   .then(() => console.log('Connected to DB!'))
   .catch(error => console.log(error));
+
 const app = express();
-const appUrl = 'https://arjcrs-two.vercel.app'
+const appUrl = 'https://arjcrs-two.vercel.app';
 
 app.use(helmet());
 app.use(logger('dev'));
-app.use(cors(
-//   {
-//     origin: [appUrl],
-//     methods: ["POST", "GET","PUT"],
-//     credentials: true
-// }
-));
+
+// Enable CORS
+app.use(cors({
+  origin: appUrl, // Allow requests from this origin
+  methods: ["POST", "GET", "PUT","DELETE","PATCH"], // Allow these HTTP methods
+  credentials: true // Allow credentials (cookies, authorization headers, etc.)
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
@@ -35,26 +37,28 @@ app.use('/api/companies', companiesRouter);
 app.use('/api/students', studentsRouter);
 app.use('/api/jobs', jobsRouter);
 app.use('/api/profile', profileRouter);
-//Catching 404 Error
-app.use((req, res, next) => {
-  const error = new Error('INVALID ROUTE')
-  error.status = 404
-  next(error);
-})
 
-//Error handler function
+// Catching 404 Error
+app.use((req, res, next) => {
+  const error = new Error('INVALID ROUTE');
+  error.status = 404;
+  next(error);
+});
+
+// Error handler function
 app.use((error, req, res, next) => {
   res.status(error.status || 500);
   res.json({
-      error: {
-          message: error.message
-      }
-  })
-})
+    error: {
+      message: error.message
+    }
+  });
+});
 
-app.use('/',(req,res)=>{
-  res.status(200).json(_response)
-})
+app.use('/', (req, res) => {
+  res.status(200).json(_response);
+});
 
 app.use((req, res) => res.status(404).send('404 - Not Found'));
+
 module.exports = app;
