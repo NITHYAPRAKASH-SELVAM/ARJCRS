79c82a6c29c3e7a58f4e9aaf72433e2de59f321b
praveen9655 <praveen965592@gmail.com>
2024-04-19 07:26:18 +0530

    U2

--- server/src/app.js
+++ server/src/app.js
@@ -16,16 +16,16 @@ mongoose
   .then(() => console.log('Connected to DB!'))
   .catch(error => console.log(error));
 const app = express();
-const appUrl = 'https://arjcrc.vercel.app'
+const appUrl = 'https://arjcrs-two.vercel.app'
 
 app.use(helmet());
 app.use(logger('dev'));
 app.use(cors(
-  {
-    origin: [appUrl],
-    methods: ["POST", "GET","PUT"],
-    credentials: true
-}
+//   {
+//     origin: [appUrl],
+//     methods: ["POST", "GET","PUT"],
+//     credentials: true
+// }
 ));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -35,7 +35,26 @@ app.use('/api/companies', companiesRouter);
 app.use('/api/students', studentsRouter);
 app.use('/api/jobs', jobsRouter);
 app.use('/api/profile', profileRouter);
+//Catching 404 Error
+app.use((req, res, next) => {
+  const error = new Error('INVALID ROUTE')
+  error.status = 404
+  next(error);
+})
+
+//Error handler function
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+      error: {
+          message: error.message
+      }
+  })
+})
+
+app.use('/',(req,res)=>{
+  res.status(200).json(_response)
+})
 
 app.use((req, res) => res.status(404).send('404 - Not Found'));
-
 module.exports = app;
